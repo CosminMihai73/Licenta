c875bac0e2e54e91c77fd1574757912b19578083
CosminMihai73 <daiacosmin666@gmail.com>
2024-04-01 12:18:22 +0300

    problema 422

--- UI/src/Pagini/QuestionsPage.js
+++ UI/src/Pagini/QuestionsPage.js
@@ -5,7 +5,7 @@ import './QuestionsPage.css';
 const QuestionsPage = () => {
   const [questions, setQuestions] = useState([]);
   const [email, setEmail] = useState('');
-  const [tempResponses, setTempResponses] = useState({});
+  const [tempResponses, setTempResponses] = useState({ raspunsuri: [], email: '' });
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const [timeRemaining, setTimeRemaining] = useState(0);
@@ -19,9 +19,6 @@ const QuestionsPage = () => {
         const response = await axios.get(`http://localhost:8000/questions?page=${currentPage}`);
         setQuestions(response.data.questions);
         setTotalPages(response.data.total_pages);
-
-        const currentQuestionTimer = response.data.questions[currentPage - 1]?.timer || 0;
-        setTimeRemaining(currentQuestionTimer);
       } catch (error) {
         console.error('Error:', error);
       }
@@ -40,26 +37,19 @@ const QuestionsPage = () => {
     }
   }, [showTimer, timeRemaining]);
 
-  const handleResponseChange = (id, response) => {
-    if (tempResponses.hasOwnProperty(id)) {
-      return;
+  useEffect(() => {
+    // Start timer automatically on page load for pages other than the first one
+    if (currentPage !== 1) {
+      startTimerAutomatically();
     }
+  }, [currentPage]);
 
-    setTempResponses(prevResponses => ({
-      ...prevResponses,
-      [id]: response
-    }));
-
-    const allQuestionsAnswered = questions.every(question => tempResponses.hasOwnProperty(question.id));
-    if (allQuestionsAnswered) {
-      goToNextPage();
-    } else {
-      const nextQuestionIndex = questions.findIndex(question => !tempResponses.hasOwnProperty(question.id));
-      if (nextQuestionIndex !== -1) {
-        const nextQuestionTimer = questions[nextQuestionIndex]?.timer || 0;
-        setTimeRemaining(nextQuestionTimer);
-        setShowTimer(true);
-      }
+  const startTimerAutomatically = () => {
+    const currentQuestion = questions.find(question => !tempResponses.raspunsuri.some(response => response.id === question.id));
+    if (currentQuestion) {
+      const questionTimer = currentQuestion.timer || 0;
+      setShowTimer(true);
+      setTimeRemaining(questionTimer);
     }
   };
 
@@ -69,48 +59,64 @@ const QuestionsPage = () => {
 
   const handleEmailValidation = () => {
     setEmailValidated(true);
-    setShowTimer(true);
     setShowEmailValidationButton(false);
+    setShowTimer(true);
+    const firstQuestionTimer = questions[0]?.timer || 0;
+    setTimeRemaining(firstQuestionTimer);
+    setTempResponses(prevState => ({ ...prevState, email: email }));
   };
 
- const goToNextPage = () => {
-  const allQuestionsAnswered = questions.every(question => tempResponses.hasOwnProperty(question.id));
-  if (allQuestionsAnswered) {
-    setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
-    window.scrollTo(0, 0);
-    resetTimerForNextPage();
-  } else {
-    alert('Te rugăm să completezi toate întrebările înainte de a trece la pagina următoare.');
-  }
-};
+  const handleResponseChange = (id, response, index) => {
+    setTempResponses(prevState => ({
+      ...prevState,
+      raspunsuri: [
+        ...prevState.raspunsuri,
+        {
+          id: id,
+          categorie: questions[index].categorie,
+          raspuns: response
+        }
+      ]
+    }));
 
-const resetTimerForNextPage = () => {
-  const nextPageQuestions = questions.filter(question => question.page === currentPage + 1);
-  if (nextPageQuestions.length > 0) {
-    const nextQuestionId = Math.min(...nextPageQuestions.map(question => question.id));
-    const nextQuestionTimer = nextPageQuestions.find(question => question.id === nextQuestionId)?.timer || 0;
-    setTimeRemaining(nextQuestionTimer);
-    setShowTimer(true);
-  }
-};
+    const allQuestionsAnswered = questions.every(question => tempResponses.raspunsuri.some(response => response.id === question.id));
+    if (allQuestionsAnswered) {
+      localStorage.setItem('tempResponses', JSON.stringify(tempResponses));
+      goToNextPage();
+    } else {
+      const nextQuestionIndex = questions.findIndex(question => !tempResponses.raspunsuri.some(response => response.id === question.id));
+      if (nextQuestionIndex !== -1) {
+        const nextQuestionTimer = questions[nextQuestionIndex]?.timer || 0;
+        setShowTimer(true);
+        setTimeRemaining(nextQuestionTimer);
+      }
+    }
+  };
+
+  const goToNextPage = () => {
+    const allQuestionsAnswered = questions.every(question => tempResponses.raspunsuri.some(response => response.id === question.id));
+    if (allQuestionsAnswered) {
+      setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
+      window.scrollTo(0, 0);
+      setTempResponses({ raspunsuri: [], email: '' });
+      setShowTimer(false);
+      setEmailValidated(false);
+    } else {
+      alert('Te rugăm să completezi toate întrebările înainte de a trece la pagina următoare.');
+    }
+  };
 
   const handleSubmit = async () => {
-    const allQuestionsAnswered = questions.every(question => tempResponses.hasOwnProperty(question.id));
+    const allQuestionsAnswered = questions.every(question => tempResponses.raspunsuri.some(response => response.id === question.id));
     if (!allQuestionsAnswered) {
       alert('Te rugăm să completezi toate întrebările înainte de a trimite răspunsurile.');
       return;
     }
 
     try {
-      const formattedResponses = Object.keys(tempResponses).map(id => ({
-        id,
-        categorie: 'string',
-        raspuns: tempResponses[id]
-      }));
-
       const payload = {
-        raspunsuri: formattedResponses,
-        email: email
+        raspunsuri: tempResponses.raspunsuri,
+        email: tempResponses.email
       };
 
       await axios.post('http://localhost:8000/actualizeaza_si_calculeaza_punctaje', payload);
@@ -138,7 +144,7 @@ const resetTimerForNextPage = () => {
             <div className="question-container">
               <p className={`question-text-${index}`}>{question.text}</p>
               {question.image_url && <img src={question.image_url} alt={`Imagine asociată întrebării ${index}`} className={`question-image-${index}`} />}
-              {showTimer && timeRemaining > 0 && index === Object.keys(tempResponses).length && (
+              {showTimer && timeRemaining > 0 && index === tempResponses.raspunsuri.length && (
                 <div className="timer-container">
                   <p className="timer-text">Timp rămas pentru această întrebare: {Math.floor(timeRemaining / 60)} minute și {timeRemaining % 60} secunde</p>
                 </div>
@@ -150,10 +156,10 @@ const resetTimerForNextPage = () => {
                       type="radio"
                       name={`response-${question.id}`}
                       value={response.value}
-                      checked={tempResponses[question.id] === response.value}
-                      onChange={() => handleResponseChange(question.id, response.value)}
+                      checked={tempResponses.raspunsuri.some(resp => resp.id === question.id && resp.raspuns === response.value)}
+                      onChange={() => handleResponseChange(question.id, response.value, index)}
                       className={`response-input-${subIndex}`}
-                      disabled={tempResponses.hasOwnProperty(question.id)}
+                      disabled={tempResponses.raspunsuri.some(resp => resp.id === question.id)}
                     />
                     <span className={`custom-radio-${subIndex}`}></span>
                     <span className={`response-value-${subIndex}`}>{response.value}</span>
